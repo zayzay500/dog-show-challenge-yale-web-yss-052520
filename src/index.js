fb13f99f558496222842f1e8ac188e8bf9e23b3b
zayzay500 <lindsay.pierce@yale.edu>
2020-07-13 17:38:23 -0400

    made add dog function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    const tableBody = document.getElementById('table-body')
+    
+    function makeEditButton() {
+        const editData = document.createElement('td')
+        const editButton = document.createElement('button')
+        editButton.innerText = 'Edit'
+        editData.append(editButton)
+        return editData
+    }
+    
+    function dogName(dog) {
+        const dogName = document.createElement('td')
+        dogName.innerText = dog.name
+        return dogName
+    }
+    
+    function dogBreed(dog) {
+        const dogBreed = document.createElement('td')
+        dogBreed.innerText = dog.breed
+        return dogBreed
+    }
+    
+    function dogSex(dog) {
+        const dogSex = document.createElement('td')
+        dogSex.innerText = dog.sex
+        return dogSex
+    }
+    
+    function addDog(dog) {
+        const tableRow = document.createElement('tr')
+        const dogName = dogName(dog)
+        const dogBreed = dogBreed(dog)
+        const dogSex = dogSex(dog)
+        const editData = makeEditButton()
+        tableRow.append(dogName, dogBreed, dogSex, editData)
+        tableBody.append(tableRow)
+    }
 })
